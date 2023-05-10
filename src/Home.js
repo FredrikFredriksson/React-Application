657f92609c0d0aea1654c6faff321b0067f8d8c7
Fredrik Fredriksson <Fredrikfredriksson93@gmail.com>
2023-05-10 21:23:11 +0200

    added blog posts

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,20 @@ import { useState } from 'react';
 
 const Home = () => {
   // let name = 'Freek';
-  const [name, setName] = useState('Freek');
-  const [age, setAge] = useState(25);
-  
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...' , author: 'Freek', id: 1 }, 
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Jenkan', id: 2 }, 
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Freek', id: 3 }
+  ]);
 
-  const handleClick = () => {
-    setName('Jenkan');
-    setAge(30);
-  }
-  
   return ( 
     <div className="home">
-      <h2>Names</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (  
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
    );
 }  
